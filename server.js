a65311082f8f38c6b2b334b1734a4c204e1f9dc3
Jorgepro89 <0237032@up.edu.mx>
2025-03-28 13:39:10 -0600

    Adaptación de codigo para uso de HTTPS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 443;
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
